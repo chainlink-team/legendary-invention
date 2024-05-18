f515d2bc3988934b3cd0c564ac7da7bca1c3ba4e
Tyson Do <doraeminemon@gmail.com>
2024-05-18 11:34:18 +0700

    Sponsor section

---
+++ src/app/_components/SponsorSection.jsx
@@ -0,0 +1,16 @@
+export default function SponsorSection() {
+  return (
+    <section id="sponsor" className="bg-white text-black py-6 pt-8">
+      <h1 className="text-[32px] leading-[40px] text-center font-display">Sponsors</h1>
+      <div id="sponsors-icon" className="flex flex-row px-16 justify-between w-full items-center my-16">
+        <img src="/logos/chainlink-logo.png"></img>
+        <img src="/logos/avalanche-logo.png"></img>
+        <img src="/logos/tree-logo.png"></img>
+        <img src="/logos/scroll-logo.png"></img>
+        <img src="/logos/polygon-logo.png"></img>
+        <img src="/logos/logo-1.png"></img>
+        <img src="/logos/sun-logo.png"></img>
+      </div>
+    </section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import HeroSection from './_components/HeroSection'
 import Navbar from './_components/Navbar'
+import SponsorSection from './_components/SponsorSection'
 
 export default function Home() {
   return (
     <main className="bg-[#070005]">
       <Navbar />
       <HeroSection />
+      <SponsorSection />
+      <section id="feature"></section>
+      <section id="let'splay"></section>
+      <footer id="footer"></footer>
     </main>
   )
 }
