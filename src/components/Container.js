34a990c279b9698a51c5c08a9399b4c6b4a3971f
Tyson Do <doraeminemon@gmail.com>
2024-05-23 18:00:54 +0700

    Move container to components folder

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-const Container = ({ children, heading }) => {
+export default function Container({ children, heading }) {
   return (
     <div className="text-white my-6 p-0.5 w-full rounded-lg magic-gradient flex flex-col">
       <div className="w-full px-6  py-10 h-full bg-[#1F0317] rounded-lg">
@@ -8,5 +8,3 @@ const Container = ({ children, heading }) => {
     </div>
   );
 };
-
-export default Container;
